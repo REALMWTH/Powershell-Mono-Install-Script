3c6f1a6b32d25379709cfff9fd1d64e7cdb3eb13
REALM <realm_bmc@hotmail.com>
2024-02-25 23:38:18 +0300

    Update install_scpsl_dependencies.ps1

--- install_scpsl_dependencies.ps1
+++ install_scpsl_dependencies.ps1
@@ -25,7 +25,7 @@ if (test-path $temp_dir) {[void](Remove-Item $temp_dir -Recurse -Confirm:$false 
 
 Write-Host "DirectX Redist (June 2010)"
 $directx = "$temp_dir\dxwebsetup.exe"
-(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/1/7/1/1718CCC4-6315-4D8E-9543-8E28A4E18C4C/dxwebsetup.exe', $directx)
+(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/8/4/A/84A35BF1-DAFE-4AE8-82AF-AD2AE20B6B14/directx_Jun2010_redist.exe', $directx)
 cmd /c start /wait $directx /Q
 del $directx
 
@@ -34,10 +34,10 @@ $Redists_unsupported = Get-VcList -Export Unsupported | Where-Object { $_.Releas
 $Redists = Get-VcList -Release 2012, 2013, 2015, 2017, 2019, 2022 | Save-VcRedist -Path $temp_dir | Install-VcRedist -Silent
 
 Write-Host "Mono 6.12.0 Stable (6.12.0.206)"
-$MonoPathx86 = "$temp_dir\mono-6.12.0.206-gtksharp-2.12.45-win32-0.msi"
-$MonoPathx64 = "$temp_dir\mono-6.12.0.206-x64-0.msi"
-(New-Object Net.WebClient).DownloadFile('https://download.mono-project.com/archive/6.12.0/windows-installer/mono-6.12.0.206-gtksharp-2.12.45-win32-0.msi', $MonoPathx86)
-(New-Object Net.WebClient).DownloadFile('https://download.mono-project.com/archive/6.12.0/windows-installer/mono-6.12.0.206-x64-0.msi', $MonoPathx64)
+$MonoPathx86 = "$temp_dir\mono-latest-x86-stable.msi"
+$MonoPathx64 = "$temp_dir\mono-latest-x64-stable.msi"
+(New-Object Net.WebClient).DownloadFile('https://download.mono-project.com/archive/mono-latest-x86-stable.msi', $MonoPathx86)
+(New-Object Net.WebClient).DownloadFile('https://download.mono-project.com/archive/mono-latest-x64-stable.msi', $MonoPathx64)
 cmd /c start /wait msiexec /i "$MonoPathx86" /q
 del $MonoPathx86
 cmd /c start /wait msiexec /i "$MonoPathx64" /q
